10e0ba0ce955a28fc77418be4ddc18ec5f6fbcd5
taeyuls <startez03@gmail.com>
2025-04-03 04:19:14 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Box from "./component/Box";
 
 const choice = {
   stone: {
-    name: "Stone",
+    name: "stone",
     img: "images/stone.jpg",
   },
   scissor: {
-    name: "Scissor",
+    name: "scissor",
     img: "images/scissor.jpg",
   },
   bo: {
-    name: "Bo",
+    name: "bo",
     img: "images/bo.jpg",
   },
 };
@@ -20,7 +20,7 @@ const choice = {
 function App() {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
-  const [result, setResult] = useState(" ");
+  const [result, setResult] = useState(null);
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
@@ -29,38 +29,42 @@ function App() {
   };
 
   const judgement = (user, computer) => {
-    console.log("user", user, "computer", computer);
+    const userName = user.name.toLowerCase();
+    const computerName = computer.name.toLowerCase();
 
-    if (user.name === computer.name) {
+    if (userName === computerName) {
       return "tie";
-    } else if (user.name === "stone")
-      return computer.name === "Scissors" ? "win" : "lose";
-    else if (user.name === "scissors")
-      return computer.name === "Bo" ? "win" : "lose";
-    else if (user.name === "bo")
-      return computer.name === "Stone" ? "win" : "lose";
+    } else if (userName === "stone")
+      return computerName === "scissor" ? "win" : "lose";
+    else if (userName === "scissor")
+      return computerName === "bo" ? "win" : "lose";
+    else if (userName === "bo")
+      return computerName === "stone" ? "win" : "lose";
   };
 
   const randomChoice = () => {
     let itemArray = Object.keys(choice);
-    console.log("item array", itemArray);
-    let randomItem = Math.floor(Math.random() * itemArray.length);
-    console.log("random value", randomItem);
-    let final = itemArray[randomItem];
-    return choice[final];
+    let randomIndex = Math.floor(Math.random() * itemArray.length);
+
+    return choice[itemArray[randomIndex]];
   };
 
   return (
     <>
       <div className="main">
-        <Box title="You" item={userSelect} result={result} />
-        <Box title="Computer" item={computerSelect} />
+        <Box title="Me" item={userSelect} result={result} />
+        <Box title="You" item={computerSelect} />
       </div>
 
       <div className="main button">
         <button onClick={() => play("scissor")}>가위</button>
         <button onClick={() => play("stone")}>바위</button>
         <button onClick={() => play("bo")}>보</button>
+      </div>
+      <div className={`result-message ${result || ""}`}>
+        {result === "win" && "🎉 이겼다!"}
+        {result === "lose" && "😢 졌다..."}
+        {result === "tie" && "😐 비겼다!"}
       </div>
     </>
   );
