25656777e822a34c65a6fe10ce5d026173b74101
taeyuls <startez03@gmail.com>
2025-04-19 03:01:24 +0900

    netflix-api-set

--- netflix-demo/src/App.js
+++ netflix-demo/src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./layout/AppLayout";
 import Homepage from "./pages/Homepage/Homepage";
+import MovieDetail from "./pages/MovieDetail/MovieDetail";
+import MoviePage from "./pages/Movies/Movies";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 // 홈페이지 /
 // 영화 전체보여주는 페이지 (서치) /movies
@@ -12,7 +15,13 @@ function App() {
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Homepage />} />
+        <Route path="/movies">
+          <Route index element={<MoviePage />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+        </Route>
       </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
--- netflix-demo/src/layout/AppLayout.jsx
+++ netflix-demo/src/layout/AppLayout.jsx
@@ -1,7 +1,82 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  return (
+    <div style={{ backgroundColor: "#000", minHeight: "100vh", color: "#fff" }}>
+      <Navbar expand="lg" bg="black" variant="black">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img
+              src="/images/netflix-logo.png"
+              alt="Netflix"
+              height="50"
+              style={{ marginRight: "10px" }}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 text-light"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/" className="text-light">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/movies" className="text-light">
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                style={{
+                  width: "15vw",
+                  height: "40px",
+                  backgroundColor: "#333",
+                  color: "#fff",
+                  border: "1px solid #555",
+                }}
+              />
+              <Button
+                variant="outline-light"
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  padding: "0",
+                  border: "1px solid red",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "transparent",
+                }}
+              >
+                <i
+                  className="bi bi-search"
+                  style={{ fontSize: "1rem", color: "red" }}
+                ></i>
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <main className="px-4 py-4">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default AppLayout;
